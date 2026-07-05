522524626e5cda547dc1be4ca2f9990ff6e8d333
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-07-05 19:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+
+const titleElement = document.getElementById("title");
+const authorElement = document.getElementById("author");
+const isbnElement = document.getElementById("isbn");
+const buttonElement=document.getElementById("submit");
+const tableElement = document.getElementById("book-list");
+
+
+buttonElement.addEventListener("click",function(e){
+
+	e.preventDefault()
+	
+	let row = document.createElement("tr");
+	let newCell1 = document.createElement("td");
+	let newCell2 = document.createElement("td");
+	let newCell3 = document.createElement("td");
+	let deleteBtnEle = document.createElement("button");
+	
+	newCell1.textContent = titleElement.value;
+	newCell2.textContent = authorElement.value;
+	newCell3.textContent = isbnElement.value;
+
+	deleteBtnEle.textContent = "X";
+	deleteBtnEle.style.color = "white";
+	deleteBtnEle.style.background = "red";
+	
+
+	row.appendChild(newCell1);
+	row.appendChild(newCell2);
+	row.appendChild(newCell3);
+	row.appendChild(deleteBtnEle);
+	tableElement.appendChild(row);
+
+
+	deleteBtnEle.addEventListener("click",function(){
+		row.remove();
+	})
+
+})
